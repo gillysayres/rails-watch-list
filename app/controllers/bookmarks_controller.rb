441f21dafa3efa833c8e46a4a23b660b49ff150e
Gillys Dantas <gillys.ayres@gmail.com>
2021-04-20 16:13:29 -0300

    Added list.find in create action from bookmarkscontroller

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -4,6 +4,7 @@ class BookmarksController < ApplicationController
   end
 
   def create
+  @list = List.find(params[:list_id])
   @bookmark = Bookmark.new(bookmark_params)
   @bookmark.list = @list
     if @bookmark.save
@@ -11,6 +12,13 @@ class BookmarksController < ApplicationController
     else
       render :new
     end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.delete
+
+    redirect_to list_path(@bookmark)
   end
 
   private
