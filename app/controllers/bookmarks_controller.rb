75aa1a10b8bdd6d415e83a1ec4842cebf8142b18
Gillys Dantas <gillys.ayres@gmail.com>
2021-04-20 15:43:23 -0300

    Inserted strong params to bookmarkscontroller

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -6,12 +6,16 @@ class BookmarksController < ApplicationController
   def create
   @bookmark = Bookmark.new(bookmark_params)
   @bookmark.list = @list
-  if @bookmark.save
-    redirect_to list_path(@list)
-  else
-    render :new
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
   end
-end
 
+  private
 
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+  end
 end
