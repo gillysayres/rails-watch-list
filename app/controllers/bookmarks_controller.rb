8a93b4ba882508ddf1c9dece463b9396ac999ae3
Gillys Dantas <gillys.ayres@gmail.com>
2021-04-20 16:32:04 -0300

    Fixed an error in create and destroy

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,4 +1,6 @@
 class BookmarksController < ApplicationController
+  before_action :set_bookmark, only: [:show, :destroy]
+
   def new
     @bookmark = Bookmark.new
   end
@@ -25,5 +27,9 @@ class BookmarksController < ApplicationController
 
   def bookmark_params
     params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+  end
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
   end
 end
