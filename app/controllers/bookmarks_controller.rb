ac4db321a245e9968835433965f5a80883fb9c71
Gillys Dantas <gillys.ayres@gmail.com>
2021-04-20 15:41:47 -0300

    Inserted create action to bookmarkscontroller

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,17 @@
 class BookmarksController < ApplicationController
   def new
-    @bookmkark = Bookmark.new
+    @bookmark = Bookmark.new
   end
+
+  def create
+  @bookmark = Bookmark.new(bookmark_params)
+  @bookmark.list = @list
+  if @bookmark.save
+    redirect_to list_path(@list)
+  else
+    render :new
+  end
+end
+
+
 end
